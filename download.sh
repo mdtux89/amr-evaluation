adad08af35c63c9a6a2d8ba74a2d823edae0d003
mdtux89 <mdtux89@gmail.com>
2016-10-07 13:31:53 +0100

    update download.sh and evaluation.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/mdtux89/amr-preprocessing.git
+rm -rf amr-preprocessing/.git*
 mv amr-preprocessing/* .
 rm -rf amr-preprocessing/
--- evaluation.sh
+++ evaluation.sh
@@ -15,9 +15,8 @@ cat "$1" | perl -ne 's/(\/ [a-zA-Z0-9\-][a-zA-Z0-9\-]*)-[0-9][0-9]*/\1-01/g; pri
 cat "$2" | perl -ne 's/(\/ [a-zA-Z0-9\-][a-zA-Z0-9\-]*)-[0-9][0-9]*/\1-01/g; print;' > 2.tmp
 python smatch_2.0.2/smatch.py -f 1.tmp 2.tmp
 
-
-cat "$1" | perl -ne 's/([a-z])[a-z]*([0-9]+)/\1\2/g; print;' | perl -ne 's/^#.*\n//g; print;' | perl -ne 's/_//g; print;' | perl -ne 's/[0-9]+([a-zA-Z]+)/\1/g; print;' > 1.tmp
-cat "$2" | perl -ne 's/([a-z])[a-z]*([0-9]+)/\1\2/g; print;' | perl -ne 's/^#.*\n//g; print;' | perl -ne 's/_//g; print;' | perl -ne 's/[0-9]+([a-zA-Z]+)/\1/g; print;' > 2.tmp
+cat "$1" | perl -ne 's/-(\d+(\.\d+)?)/\1/g; print;' | perl -ne 's/,/\./g; print;' | perl -ne 's/^#.*\n//g; print;' | perl -ne 's/_//g; print;' | perl -ne 's/[0-9]+([a-zA-Z]+)/\1/g; print;' > 1.tmp
+cat "$2" | perl -ne 's/-(\d+(\.\d+)?)/\1/g; print;' | perl -ne 's/,/\./g; print;' | perl -ne 's/^#.*\n//g; print;' | perl -ne 's/_//g; print;' | perl -ne 's/[0-9]+([a-zA-Z]+)/\1/g; print;' > 2.tmp
 
 python scores.py "1.tmp" "2.tmp"
 
